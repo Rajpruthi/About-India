956e2c396c8baac70d8e352d6d7453bc160082a9
PRUTHIRAJ ROUT <pruthirajrout843@gmail.com>
2026-05-15 11:59:06 +0530

    Final fix: Moving config files to root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // Only use the base path on GitHub Pages
   base: process.env.NODE_ENV === 'production' ? '/About-India/' : '/',
   server: {
     port: 5173,
